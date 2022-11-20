2ac2f6b46cffeb0229969c90155824e9c27ce051
George8080 <josantis2021@inf.udec.cl>
2022-11-20 19:55:49 -0300

    Cambios en paint

--- Vehicle/src/vehicle/Pista.java
+++ Vehicle/src/vehicle/Pista.java
@@ -11,14 +11,11 @@ import java.awt.Graphics2D;
  */
 public class Pista {
     public Pista(){
-        
     }
     
     public void paint(Graphics g){
         Graphics2D road = (Graphics2D)g;
         road.setColor(Color.gray);
         road.fillOval(30, 30, 600, 600);     
-        road.setColor(new Color(100,255,100));
-        road.fillOval(200, 200, 270, 270);
     }
 }
