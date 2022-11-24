e99d410de7dae7f9abe32a51d34628f3e82ff0e6
daninonino <danielanovoao600@gmail.com>
2022-11-24 15:45:13 -0300

    correccion de metodo paint de pista, se añade pasto en el area central

--- Vehicle/src/vehicle/Pista.java
+++ Vehicle/src/vehicle/Pista.java
@@ -15,7 +15,20 @@ public class Pista {
     
     public void paint(Graphics g){
         Graphics2D road = (Graphics2D)g;
-        road.setColor(Color.gray);
+        
+        Graphics2D solera = (Graphics2D)g;  
+         
+        solera.setColor(Color.black);   //solera externa
+        solera.fillOval(20, 20, 620, 620);     
+        
+        solera.setColor(Color.black);  //solera interna
+        solera.fillOval(200, 200, 300, 300);
+        
+        road.setColor(Color.gray);      //pista
         road.fillOval(30, 30, 600, 600);     
+        
+        road.setColor(new Color(100,255,100));    //pasto
+        road.fillOval(200, 200, 270, 270);
+        
     }
 }
