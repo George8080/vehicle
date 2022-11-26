a91c46d15cc5db61062a87ec58778a09861dfd9b
daninonino <danielanovoao600@gmail.com>
2022-11-26 14:45:39 -0300

    se cambian las definiciones del centro de las ruedas para que roten junto al auto, todavia hay errores que necesitan ser arreglados

--- Vehicle/src/vehicle/Ruedas.java
+++ Vehicle/src/vehicle/Ruedas.java
@@ -14,40 +14,45 @@ import static java.lang.Math.sin;
 public class Ruedas{
     private int centerX;
     private int centerY;
+    private int centerRadio;
     private int[] intX;
     private int[] intY;
     private double[] radio;
     private double degree=0;
     private double[] degrees;
+    private double angulo;
     
-    public Ruedas ( int x, int y) {
-        centerX = x;
-        centerY = y;
+    public Ruedas ( int cX, int x, int cY, int y, double angle) {
+        angulo = Math.toDegrees(Math.atan(y/x))+angle;
+        System.out.println(angulo);
+        centerX = (int) (cX+ Math.sqrt(x*x + y*y)*sin(Math.toRadians(angulo)));
+        centerY = (int) (cY+ Math.sqrt(x*x + y*y)*sin(Math.toRadians(angulo)));
+        //centerRadio = (int) Math.sqrt(x*x+y*y);
         radio = new double[]{Math.sqrt(41),Math.sqrt(41),Math.sqrt(41),Math.sqrt(41)};
-        degrees = new double[]{30,-30,-150,150};
+        degrees = new double[]{30+angle,-30+angle,-150+angle,150+angle};
         /*intX = new int[]{x+40, x+20, x-30, x-30, x+20};
         intY = new int[]{y, y+25, y+15, y-15, y-25};*/
         
-        intX = new int[]{0,0,0,0,0};
+        intX = new int[]{0,0,0,0};
         for(int i = 0; i < 4; i++){
-            intX[i] = (int) (x + radio[i]*cos(Math.toRadians(degrees[i])));
+            intX[i] = (int) (centerX + radio[i]*cos(Math.toRadians(degrees[i])));
         }
-        intY = new int[]{0,0,0,0,0};
+        intY = new int[]{0,0,0,0};
         for(int i = 0; i < 4; i++){
-            intY[i] = (int) (y + radio[i]*sin(Math.toRadians(degrees[i])));
+            intY[i] = (int) (centerY + radio[i]*sin(Math.toRadians(degrees[i])));
     }
     
     }
     public void setXY(int vel, double angle){
-        degree += angle;
-        centerY = (int) (centerY + vel*sin(Math.toRadians(degree)));
-        centerX = (int) (centerX + vel*cos(Math.toRadians(degree)));
+        angulo += angle;
+        centerY = (int) (centerY + vel*sin(Math.toRadians(angulo)));
+        centerX = (int) (centerX + vel*cos(Math.toRadians(angulo)));
         
         for(int i = 0; i < 4; i++){
-            intX[i] = (int) (centerX + radio[i]*cos(Math.toRadians(degrees[i]+degree)));
+            intX[i] = (int) (centerX + radio[i]*cos(Math.toRadians(degrees[i]+angulo)));
         }
         for(int i = 0; i < 4; i++){
-            intY[i] = (int) (centerY + radio[i]*sin(Math.toRadians(degrees[i]+degree)));
+            intY[i] = (int) (centerY + radio[i]*sin(Math.toRadians(degrees[i]+angulo)));
         }
     
     }
