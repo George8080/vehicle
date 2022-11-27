04740b7da07d5a93039cd3d13f8ab13afd588b2a
George8080 <josantis2021@inf.udec.cl>
2022-11-27 03:23:52 -0300

    Cambios en la generacion de un nuevo valor del grado del auto, y en los setter's a las ruedas

--- Vehicle/src/vehicle/Auto.java
+++ Vehicle/src/vehicle/Auto.java
@@ -8,12 +8,12 @@ import static java.lang.Math.*;
 
 /**
  *
- * @author jorge
+ * @author jorge y danino
  */
 public class Auto {
     private Color color;
-    private int centerX;
-    private int centerY;
+    private double centerX;
+    private double centerY;
     private int[] intX;
     private int[] intY;
     private Ruedas  upperLeft;
@@ -21,7 +21,7 @@ public class Auto {
     private Ruedas lowerLeft;
     private Ruedas lowerRight;
     private double[] radio;
-    private double degree = 45;
+    private double degree = 0;
     private double[] degrees;
 
     public Auto (Color c, int x, int y){
@@ -30,10 +30,10 @@ public class Auto {
         centerY = y;
         radio = new double[]{40,Math.sqrt(400+625),Math.sqrt(90+225),Math.sqrt(90+225),Math.sqrt(400+625)};
         degrees = new double[]{0,-40,-140,140,40};
-            lowerLeft= new Ruedas(x,-12,y,12,degree);
-            lowerRight= new Ruedas(x,-12,y,-12,degree);
-            upperLeft= new Ruedas(x,15,y,20,degree);
-            upperRight= new Ruedas(x,15,y,-20,degree);
+        lowerLeft= new Ruedas(x,-12,y,12,degree);
+        lowerRight= new Ruedas(x,-12,y,-12,degree);
+        upperLeft= new Ruedas(x,15,y,20,degree);
+        upperRight= new Ruedas(x,15,y,-20,degree);
         
         
         /*intX = new int[]{x+40, x+20, x-30, x-30, x+20};
@@ -49,18 +49,20 @@ public class Auto {
         }
     }
     
-    public void setXY(int vel, double angle){
-        degree += angle;
-        centerY = (int) (centerY + vel*sin(Math.toRadians(degree)));
-        centerX = (int) (centerX + vel*cos(Math.toRadians(degree)));
-        
+    public void setXY(double vel, double angle){
+        //if(vel!=0){
+        degree += vel*angle/3;
+        centerY =  (centerY + 3*vel*sin(Math.toRadians(degree)));
+        centerX =  (centerX + 3*vel*cos(Math.toRadians(degree)));
+
+
         for(int i = 0; i < 5; i++){
             intX[i] = (int) (centerX + radio[i]*cos(Math.toRadians(degrees[i]+degree)));
         }
         for(int i = 0; i < 5; i++){
             intY[i] = (int) (centerY + radio[i]*sin(Math.toRadians(degrees[i]+degree)));
         }
-        
+        //}
         /*for(int i = 0; i < 5; i++){
             //if(angle!=0) intY[i]+=(int)((intY[i]-centerY)*sin(Math.toRadians(degree)));
             intY[i]+=(int)(vel*sin(Math.toRadians(degree)));
@@ -69,19 +71,18 @@ public class Auto {
             //if(angle!=0) intX[i]+=(int)((intX[i]-centerX)*cos(Math.toRadians(degree)));
             intX[i]+=(int)(cos(Math.toRadians(degree))*vel);
         }*/
-        
-            lowerLeft.setXY(vel, angle);
-            lowerRight.setXY(vel, angle);
-            upperLeft.setXY(vel, angle);
-            upperRight.setXY(vel, angle);
+        lowerLeft.setXY(centerX, centerY,angle*vel/3,0);
+        lowerRight.setXY(centerX, centerY,angle*vel/3,0);
+        upperLeft.setXY(centerX, centerY, angle*vel/3,8*angle);
+        upperRight.setXY(centerX,centerY, angle*vel/3,8*angle);
     }
     
     public void paint(Graphics g){
         Graphics2D car = (Graphics2D)g;
-         lowerLeft.paint(g);
-         lowerRight.paint(g);
-         upperLeft.paint(g);
-         upperRight.paint(g);
+        lowerLeft.paint(g);
+        lowerRight.paint(g);
+        upperLeft.paint(g);
+        upperRight.paint(g);
         car.setColor(color);
         car.fillPolygon(intX, intY, 5);
         
