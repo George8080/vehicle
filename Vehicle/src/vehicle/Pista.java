a0e3becee5036d8a5db0d7f35886944990228690
George8080 <josantis2021@inf.udec.cl>
2022-12-04 17:32:11 -0300

    Creación de Variables y Metodos para alterar el tamaño de la Pista

--- Vehicle/src/vehicle/Pista.java
+++ Vehicle/src/vehicle/Pista.java
@@ -10,25 +10,65 @@ import java.awt.Graphics2D;
  * @author jorge
  */
 public class Pista {
+    private int large;
+    private int high;
+    private int width;
     public Pista(){
+        large = 0;
+        high = 0;
+        width = 175;
+        //width = 1;
     }
     
     public void paint(Graphics g){
         Graphics2D road = (Graphics2D)g;
-        
         Graphics2D solera = (Graphics2D)g;  
          
         solera.setColor(Color.black);   //solera externa
-        solera.fillOval(20, 20, 620, 620);     
+        solera.fillOval(large, high, 643-(2*large), 643-(2*high));     
         
-        solera.setColor(Color.black);  //solera interna
+        /*solera.setColor(Color.black);  //solera interna
         solera.fillOval(200, 200, 300, 300);
-        
+        */
         road.setColor(Color.gray);      //pista
-        road.fillOval(30, 30, 600, 600);     
+        road.fillOval(10+large, 10+high, 623-(2*large), 623-(2*high));  
         
         road.setColor(new Color(100,255,100));    //pasto
-        road.fillOval(200, 200, 270, 270);
+        road.fillOval(10+large/2+width, width+10+high/2, 623-(2*width)-large, 623-(2*width)-high);
+    }
+    
+    public void setSize(int x, int y, int z){
+        large += x;
+        if(large > 185)
+            large = 185;
+        else if(large < 0)
+            large = 0;
         
+        high += y;
+        if(high > 185)
+            high = 185;
+        else if(high < 0)
+            high = 0;
+        
+        width += z;
+        /*
+        if(width > 10) width = 10;
+        else if(width < 0) width = 0;
+        */
+        if(width > 190)
+            width = 190;
+        else if(width < 150)
+            width = 150;
+        
+    }
+    public void setMin(){
+        large = 185;
+        high = 185;
+        width = 190;
+    }
+    public void setMax(){
+        large = 0;
+        high = 0;
+        width = 150;
     }
 }
