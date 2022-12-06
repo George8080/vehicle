61dfc6676f44f639bca3912ced0a8204e8e6ccfe
George8080 <josantis2021@inf.udec.cl>
2022-12-06 10:38:54 -0300

    Transformación del achivo a Singleton

--- Vehicle/src/vehicle/Ventana.java
+++ Vehicle/src/vehicle/Ventana.java
@@ -5,14 +5,17 @@ import javax.swing.JFrame;
 
 public class Ventana extends JFrame{
     // Crear un nuevo panel.
+    private static Ventana vent = new Ventana();
     PanelPrincipal panel = new PanelPrincipal();
-    public Ventana(){
+    private Ventana(){
         this.setSize(660, 735);     // Establecemos el tamaño de la ventana.
         this.setResizable(false);   // Establecer su tamaño estático.
         setTitle("Vehicle");    // Definir el titulo de la Ventana como "Vehicle".
         this.add(panel);            // Añadir el panel a la ventana.
         setLocationRelativeTo(null);// Posicionar la ventana en el centro.
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);    // Cierra la ventana
-        
+    }
+    public static Ventana getInstancia( ){
+        return  vent;
     }
 }
