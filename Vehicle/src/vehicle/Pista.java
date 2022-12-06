a3c50466281720939416cfb198d5c7da6cfa4a5b
George8080 <josantis2021@inf.udec.cl>
2022-12-06 10:37:18 -0300

    Cambio en los limites del grosor de la pista + Cambio de color a los bordes

--- Vehicle/src/vehicle/Pista.java
+++ Vehicle/src/vehicle/Pista.java
@@ -24,17 +24,21 @@ public class Pista {
         Graphics2D road = (Graphics2D)g;
         Graphics2D solera = (Graphics2D)g;  
          
-        solera.setColor(Color.black);   //solera externa
+        solera.setColor(Color.darkGray);   //solera externa
         solera.fillOval(large, high, 643-(2*large), 643-(2*high));     
         
-        /*solera.setColor(Color.black);  //solera interna
+        /*solera.setColor(Color.black);  //solera externa
         solera.fillOval(200, 200, 300, 300);
         */
         road.setColor(Color.gray);      //pista
         road.fillOval(10+large, 10+high, 623-(2*large), 623-(2*high));  
         
-        road.setColor(new Color(100,255,100));    //pasto
+        
+        road.setColor(Color.darkGray);    //solera interna
         road.fillOval(10+large/2+width, width+10+high/2, 623-(2*width)-large, 623-(2*width)-high);
+        
+        road.setColor(new Color(100,255,100));    //pasto
+        road.fillOval(20+large/2+width, width+20+high/2, 603-(2*width)-large, 603-(2*width)-high);
     }
     
     public void setSize(int x, int y, int z){
@@ -55,20 +59,20 @@ public class Pista {
         if(width > 10) width = 10;
         else if(width < 0) width = 0;
         */
-        if(width > 190)
+        if(width > 195)
             width = 190;
-        else if(width < 150)
-            width = 150;
+        else if(width < 160)
+            width = 160;
         
     }
     public void setMin(){
         large = 185;
         high = 185;
-        width = 190;
+        width = 195;
     }
     public void setMax(){
         large = 0;
         high = 0;
-        width = 150;
+        width = 160;
     }
 }
